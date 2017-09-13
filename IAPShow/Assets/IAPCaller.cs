fd5702c1c16c995180d944f110ff161288a22506
Freist Li <freistli@microsoft.com>
2017-09-13 13:40:02 +0800

    Add unity sample

---
+++ IAPShow/Assets/IAPCaller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IAPCaller : MonoBehaviour
+{
+    [DllImport("IAPWrapper")]
+    extern static IntPtr Purchase(string storeID);
+    
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        TaskOnClick();
+    }
+     void TaskOnClick()
+    {
+        Purchase("9pjp4testztd");
+        Debug.Log("You have clicked the mouse!");
+    }
+}
