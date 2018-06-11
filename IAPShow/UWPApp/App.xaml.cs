a440544f5a85ddd84613f5842f7749fd54173ced
Freist Li <freistli@microsoft.com>
2018-06-11 14:28:40 +0800

    Add sample code for calling UWP Plugin in built UWP Unity

---
+++ IAPShow/UWPApp/App.xaml.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using UnityPlayer;
+using IAPUWPDLL;
+using Windows.ApplicationModel.Core;
+using UnityEngine;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=234227
+
+namespace IAPShow
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Windows.UI.Xaml.Application
+    {
+        private AppCallbacks appCallbacks;
+        public SplashScreen splashScreen;
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            SetupOrientation();
+            appCallbacks = new AppCallbacks();
+        }
+
+        /// <summary>
+        /// Invoked when application is launched through protocol.
+        /// Read more - http://msdn.microsoft.com/library/windows/apps/br224742
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            string appArgs = "";
+
+            switch (args.Kind)
+            {
+                case ActivationKind.Protocol:
+                    ProtocolActivatedEventArgs eventArgs = args as ProtocolActivatedEventArgs;
+                    splashScreen = eventArgs.SplashScreen;
+                    appArgs += string.Format("Uri={0}", eventArgs.Uri.AbsoluteUri);
+                    break;
+            }
+            InitializeUnity(appArgs);
+
+
+        }
+
+        /// <summary>
+        /// Invoked when application is launched via file
+        /// Read more - http://msdn.microsoft.com/library/windows/apps/br224742
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnFileActivated(FileActivatedEventArgs args)
+        {
+            string appArgs = "";
+
+            splashScreen = args.SplashScreen;
+            appArgs += "File=";
+            bool firstFileAdded = false;
+            foreach (var file in args.Files)
+            {
+                if (firstFileAdded) appArgs += ";";
+                appArgs += file.Path;
+                firstFileAdded = true;
+            }
+
+            InitializeUnity(appArgs);
+
+
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used when the application is launched to open a specific file, to display
+        /// search results, and so forth.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            splashScreen = args.SplashScreen;
+            InitializeUnity(args.Arguments);
+
+        }
+
+        private void InitializeUnity(string args)
+        {
+            ApplicationView.GetForCurrentView().SuppressSystemOverlays = true;
+
+            appCallbacks.SetAppArguments(args);
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null && !appCallbacks.IsInitialized())
+            {
+                rootFrame = new Frame();
+                Window.Current.Content = rootFrame;
+#if !UNITY_HOLOGRAPHIC
+                Window.Current.Activate();
+#endif
+                rootFrame.Navigate(typeof(MainPage));
+            }
+
+            Window.Current.Activate();
+
+            //Add this statement to allow Unity code call IAP code on UWP UI thread
+            AppCallbacks.Instance.InvokeOnAppThread(new AppCallbackItem(() =>
+            {
+                 IAPCaller.callIAP
+                    = new IAPCaller.CallIAP(CallIAP);
+            }), false);
+
+        }
+        public async void CallIAP()
+        {
+            String result = String.Empty;
+
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.High, async () =>
+            {
+                IAPWrapper iapwrapper = new IAPWrapper();
+                result = await iapwrapper.Purchase("test").ConfigureAwait(true);
+
+                var dialog = new MessageDialog(result, "消息提示");
+
+                dialog.Commands.Add(new UICommand("确定", cmd => { }, commandId: 0));
+                dialog.Commands.Add(new UICommand("取消", cmd => { }, commandId: 1));
+
+                //设置默认按钮，不设置的话默认的确认按钮是第一个按钮
+                dialog.DefaultCommandIndex = 0;
+                dialog.CancelCommandIndex = 1;
+
+                //获取返回值
+                var result2 = await dialog.ShowAsync();
+
+            }
+            );
+            Debug.Log(result);
+        }
+        void SetupOrientation()
+        {
+            Unity.UnityGenerated.SetupDisplay();
+        }
+    }
+}
